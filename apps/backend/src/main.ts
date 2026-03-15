bafdfddfbb2b82f1d003c8abe2980d6dced872d7
suyeoniii <ssy4230@naver.com>
2026-03-15 23:31:39 +0900

    chore: add allowed origin

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,7 +8,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN ?? [
+      'http://localhost:5173',
+      'https://coffee-diary-app.onrender.com',
+    ],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
